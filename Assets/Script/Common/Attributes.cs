cc6d0b1e8a76e3475fec3bff146d79e8dc1bbd95
Nguyễn Xuân Giáp <giapnx.ict@gmail.com>
2016-04-19 01:49:46 +0700

    Hoàn thành chức năng của Layout MainMenu + Select

---
+++ Assets/Script/Common/Attributes.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Attributes {
+
+	public static int mapID;		// 1, 2 or 3
+	public static int difficulty;	// 1-de, 2-trung binh, 3-kho
+
+}
