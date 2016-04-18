bb5211b1b382ebef63507918534b7a71a9ec2df4
haileader <pham.haisv@gmail.com>
2016-04-18 10:05:36 +0700

    * Player bắn đạn

--- Assets/Script/Player/ShootEnemy.cs
+++ Assets/Script/Player/ShootEnemy.cs
@@ -17,8 +17,21 @@ public class ShootEnemy : MonoBehaviour {
     void Update() {
         GameObject target = null;
 
+        //TODO: set target object cho bullet o day.
         float minimalEnemyDistance = float.MaxValue;
-        foreach (GameObject enemy in enemiesInRange) { }
+        //foreach (GameObject enemy in enemiesInRange) { }
+
+        if (target != null) {
+            if (Time.time - lastShotTime > playerData.CurrentLevel.fireRate) {
+                Shoot(target.GetComponent<Collider2D>());
+                lastShotTime = Time.time;
+            }
+            //Xoay player
+            Vector3 direction = gameObject.transform.position - target.transform.position;
+            gameObject.transform.rotation = Quaternion.AngleAxis(
+                Mathf.Atan2(direction.y, direction.x) * 180 / Mathf.PI,
+                new Vector3(0, 0, 1));
+        }
     }
 
     void Shoot(Collider2D target) {
@@ -38,4 +51,20 @@ public class ShootEnemy : MonoBehaviour {
 
         //Animator & audio shot
     }
+
+    void OnEnemyDestroy(GameObject enemy)
+    {
+        enemiesInRange.Remove(enemy);
+    }
+
+    /*void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag.Equals("Enemy"))
+        {
+            enemiesInRange.Add(other.gameObject);
+            EnemyDestructionDelegate del =
+                other.gameObject.GetComponent<EnemyDestructionDelegate>();
+            del.enemyDelegate += OnEnemyDestroy;
+        }
+    }*/
 }
