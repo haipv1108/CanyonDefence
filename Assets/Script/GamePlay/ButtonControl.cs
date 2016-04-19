20163fe747ff27c7622c21785f4e800651f079ec
haileader <pham.haisv@gmail.com>
2016-04-19 21:22:19 +0700

    * Find child object

---
+++ Assets/Script/GamePlay/ButtonControl.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonControl : MonoBehaviour {
+
+    public GameObject pauseObject;
+    public GameObject x1Object;
+    public GameObject soundObject;
+    public Popup notifyPopupObject;
+
+    public void Pause() {
+        //notifyPopupObject.Open();
+        //Change sprite
+        pauseObject.transform.GetChild(0).gameObject.SetActive(true);
+    }
+}
---
+++ Assets/Script/GamePlay/Popup.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Popup : MonoBehaviour {
+
+
+    public void Open() {
+        gameObject.SetActive(true);        
+    }
+
+    public void Close() {
+        gameObject.SetActive(false);
+    }
+}
