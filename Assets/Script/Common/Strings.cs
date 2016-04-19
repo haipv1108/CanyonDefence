df7bc890b9abb086ad0ae7f41aba31832337b603
haileader <pham.haisv@gmail.com>
2016-04-19 17:14:38 +0700

    + Back Controller

---
+++ Assets/Script/Common/Strings.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Strings : MonoBehaviour {
+
+    public const string SCEN_MENU = "MainMenu";
+    public const string SCEN_SELECTMAP = "SelectMap";
+    public const string SCEN_GAMEPLAY = "GamePlay";
+}
---
+++ Assets/Script/GameManager/PauseController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseController : MonoBehaviour {
+
+    public GameObject selectDifficulty;
+    public SelectController selectController;
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (selectDifficulty.activeSelf)
+            {
+                selectController.BackSelectMap();
+                Debug.Log("select map : true");
+            }
+            else {
+                //Back to menu scene
+                selectController.BackMainMenu();
+                Debug.Log("select map: false");
+            }
+        }
+    }
+}
