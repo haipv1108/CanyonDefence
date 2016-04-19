1c0ff267da9c68894522db12498e75d14d600bcf
haileader <pham.haisv@gmail.com>
2016-04-19 23:29:42 +0700

    + Add setting to manu

--- Assets/Script/Layout/MainMenuController.cs
+++ Assets/Script/Layout/MainMenuController.cs
@@ -3,28 +3,34 @@ using System.Collections;
 
 public class MainMenuController : MonoBehaviour {
 
-	public GameObject BgIn;
-	public GameObject HelpCanvas;
-	public GameObject ScoreCanvas;
+    public GameObject BgIn;
+    public GameObject HelpCanvas;
+    public GameObject ScoreCanvas;
+    public GameObject SettingCanvas;
 
-	// Use this for initialization
-	void Start () {
-		BgIn.SetActive (true);
-		HelpCanvas.SetActive (false);
-		ScoreCanvas.SetActive (false);
-	}
+    // Use this for initialization
+    void Start() {
+        BgIn.SetActive(true);
+        HelpCanvas.SetActive(false);
+        ScoreCanvas.SetActive(false);
+        SettingCanvas.SetActive(false);
+    }
 
-	// Click Button
-	public void PlayBtn() {
-		Application.LoadLevel (Strings.SCEN_SELECTMAP);
-	}
+    // Click Button
+    public void PlayBtn() {
+        Application.LoadLevel(Strings.SCEN_SELECTMAP);
+    }
 
-	public void HelpBtn() {
-//		print ("Click Help Btn");
-		HelpCanvas.SetActive (true);
-	}
+    public void HelpBtn() {
+        //		print ("Click Help Btn");
+        HelpCanvas.SetActive(true);
+    }
 
-	public void ScoreBtn() {
-		ScoreCanvas.SetActive (true);
-	}
+    public void ScoreBtn() {
+        ScoreCanvas.SetActive(true);
+    }
+
+    public void SettingBtn() {
+        SettingCanvas.SetActive(true);
+    }
 }
