c1430779a84b052315d347504aa4bc6567868668
haileader <pham.haisv@gmail.com>
2016-04-19 18:05:27 +0700

    * Edit playerinfo

--- Assets/Script/Player/ViewListPlayer/PlayerItemInfo.cs
+++ Assets/Script/Player/ViewListPlayer/PlayerItemInfo.cs
@@ -7,10 +7,27 @@ public class PlayerItemInfo : MonoBehaviour {
     public Text goldText;
     public Text descriptionText;
     public Image image;
+    public GameObject descriptionObject;
+    public Text descriptionButtonText;
 
     public void LoadPlayerInfo(PlayerInfo playerInfo) {
         this.goldText.text = playerInfo.gold.ToString();
         this.descriptionText.text = playerInfo.description;
         this.image.sprite = playerInfo.sprite;
     }
+
+    public void ActiveDescription() {
+        if (!descriptionObject.activeSelf)
+        {
+            descriptionObject.SetActive(true);
+            descriptionButtonText.text = "[X]";
+            descriptionButtonText.color = Color.red;
+        }
+        else {
+            descriptionObject.SetActive(false);
+            descriptionButtonText.text = "[?]";
+            descriptionButtonText.color = Color.white;
+        }
+    }
+
 }
