fb3a4cae85c3ad8770e901ec52ae685ab74935c4
Nguyễn Xuân Giáp <giapnx.ict@gmail.com>
2016-05-09 18:51:59 +0700

    + help popup

---
+++ Assets/Script/Layout/HelpGamePlayController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelpGamePlayController : MonoBehaviour {
+
+	public GameObject[] HelpList;
+	private int currentID;
+
+	// Use this for initialization
+	void Start () {
+		currentID = 0;
+		HelpList [0].SetActive (true);
+	}
+
+	void SetStatus(int index, bool value)
+	{
+		HelpList [index].SetActive (value);
+	}
+
+	public void ClickNextBtn()
+	{
+		if (currentID + 1 >= HelpList.Length)
+			ClickBackBtn ();
+
+		if (currentID + 1 <= HelpList.Length - 1 ) {
+			SetStatus(currentID, false);
+			currentID ++;
+			SetStatus(currentID, true);
+		}
+	}
+
+	public void ClickBackBtn()
+	{
+		if (currentID - 1 >= 0) {
+			SetStatus(currentID, false);
+			currentID --;
+			SetStatus(currentID, true);
+		}
+	}
+
+	public void ClickSkipBtn()
+	{
+		print ("Start Game................");
+	}
+}
